ba10de17ccf04b87172163ad5106581ff01b1505
briangor <briangor13@gmail.com>
2024-03-29 21:20:49 +0300

    add server msg and quotes api

--- src/App.js
+++ src/App.js
@@ -9,23 +9,49 @@ function App() {
   const [items, setItems] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [query, setQuery] = useState('');
+  const [serverErr, setServerErr] = useState(false);
+  const [quotes, setQuotes] = useState([]);
 
 
   useEffect(() => {
     const fetchItems = async () => {
-      const result = await axios(`https://www.breakingbadapi.com/api/characters?name=${query}`);
+      try {
+        const result = await axios(`https://www.breakingbadapi.com/api/characters?name=${query}`);
 
-      setItems(result.data);
-      setIsLoading(false);
+        setItems(result.data);
+        setIsLoading(false);
+      } catch (error) {
+        console.log(`${error.name}: There is a ${error.message}`);
+        setServerErr(true);
+      }
     }
     fetchItems();
   }, [query]);
 
+  useEffect(() => {
+    const fetchQuotes = async () => {
+      try {
+        const result = await axios.get('https://api.breakingbadquotes.xyz/v1/quotes');
+        setQuotes(result.data[0]);
+        console.log(quotes);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    fetchQuotes();
+  },[query]);
+
   return (
     <div className="container">
       <Header />
-      <Search getQuery={(q) => setQuery(q)}/>
-      <CharacterGrid isLoading={isLoading} items={items}/>
+      <Search getQuery={(q) => setQuery(q)} />
+      <CharacterGrid isLoading={isLoading} items={items} />
+      {serverErr && <p className='msg'>The server might be down :(</p>}
+      {quotes && 
+      <div className='quotes'>
+        <p>{quotes.quote}</p>
+        <p>~ {quotes.author}</p>
+      </div>}
     </div>
   );
 }
